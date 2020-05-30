a1db58071fba67b6aef7de8e7bb931f40106ef25
Nodirbek <yakubov.nodir2000@mail.ru>
2020-05-30 13:20:10 +0500

    beta version

--- src/main.js
+++ src/main.js
@@ -5,6 +5,21 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATrXiKZf3uBZ-GM1tAkrIXFzH8v6ylPDc",
+  authDomain: "vuefs-prod-2f412.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-2f412.firebaseio.com",
+  projectId: "vuefs-prod-2f412",
+  storageBucket: "vuefs-prod-2f412.appspot.com",
+  messagingSenderId: "917692270721",
+  appId: "1:917692270721:web:11073e3a85deecc2c8a580",
+  measurementId: "G-VKD3R0DDHQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
   router,
   store,
